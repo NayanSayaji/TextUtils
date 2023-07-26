53523b00daa4e38ffa97fcabe985936edba87010
Nayan <nayansayaji277@gmail.com>
2023-07-27 01:00:23 +0530

    learnt about useState Hook and event handlers in react and implemented it into the project. Added few minor functionalies into TextUtils

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
 
 function App() {
   return (
     <>
-    {/* this title ="TextUtils" is a props. props -> property  */}
-    <Navbar title ="TextUtils2"/>
-    </>  
+      {/* this title ="TextUtils" is a props. props -> property  */}
+      <Navbar title="TextUtils2" aboutText="ABOUT" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below." />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,52 @@
+// props
+// functional components
+// useState : is a hook which is used to create states
+// 
+
+
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        // console.log(newText)
+    }
+
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value);
+    }    
+
+    // its using array destructuring 
+    const [text, setText] = useState("");
+    // text = "new text"; // wrong way of changing the state
+    // setText("new text"); // correct way of changing the state
+
+    return (
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                {/* as here I am using "text" a state variable so the value of that state area is equal to the value of variable "text" so for showing the typed characters on the textarea I need to add the onchange event handeler which will update the value of input characters */}
+                <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder='Enter text here'></textarea>
+            </div>
+            <button className="btn btn-primary m-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary m-2" onClick={handleLowClick}>Convert to Lowercase</button>
+        </div>
+        <div className="container my-3">
+            <h3>Summary About the above given Text</h3>
+            <p> {text.split(" ").length} words , {text.length} characters.</p>
+            <p>{text.split(" ").length * 0.008} Minutes read.</p>
+            <h2>Preview of the text.</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    )
+}
