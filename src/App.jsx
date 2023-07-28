25be5cd447dbd6d49d542af1410af3a32b4e8839
Nayan <nayansayaji277@gmail.com>
2023-07-28 12:02:47 +0530

    added routing using react-router-dom@5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -12,7 +19,7 @@ function App() {
   const [alert, setAlert] = useState(null);
 
 
-  const showAlert =(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -23,29 +30,50 @@ function App() {
     }, 3000);
   }
 
-  const toggleMode = ()=>{
-    if (mode==='light') {
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor ="#212530f1";
-      showAlert("Dark mode has beed enabled","success")      
-    }else{
+      document.body.style.backgroundColor = "#212530f1";
+      showAlert("Dark mode has beed enabled", "success");
+      document.title = "TextUtils - Dark Mode is On";
+
+
+      // we can flash the title using setIntervals
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing !!!";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Use TextUtils it's Awesome !!!";
+      // }, 700);
+
+
+    } else {
       setMode('light');
-      document.body.style.backgroundColor ="white";
-      showAlert("Light mode has beed enabled","success")      
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has beed enabled", "success")
       // document.body.style.color ="black";
+      document.title = "TextUtils - Light Mode is On";
     }
   }
 
   return (
     <>
-      {/* this title ="TextUtils" is a props. props -> property  */}
-      <Navbar title="TextUtils" aboutText="ABOUT" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below." mode={mode}/>
-        {/* <About/> */}
-      </div>
 
+      <Router>
+        {/* this title ="TextUtils" is a props. props -> property  */}
+        <Navbar title="TextUtils" aboutText="ABOUT" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Enter the text to analyze below." mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,7 +7,7 @@ function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   }
   return (
-      props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+      props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show mx-5 p-3`} role="alert">
           <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
       </div>
   )
